d83cd66805579b33519031c4a0a0cd332dada68e
dream1ike <timur.uv@gmail.com>
2022-12-19 12:57:07 +0500

    update binom throw Pascal Triangle

--- 2. working_with_arrays/10.binom/binom.cpp
+++ 2. working_with_arrays/10.binom/binom.cpp
@@ -1,50 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #pragma warning(disable:4996)
-double factorial(int n) {
-    if (n == 0 || n == 1)return 1;
-    return factorial(n - 1) * n;
-}
-double* binom(int n)
+double get_num_from_file(const char* filename)
 {
-    double* arr = (double*)malloc(sizeof(double) * (n + 1));
-    if (arr)
+    FILE* fin = fopen(filename, "r");
+    int x = 0;
+    if (fin)
     {
-        double n_fact = factorial(n);
-        arr[0] = 1;
-        arr[n] = 1;
-        for (int k = 1; k <= n; k++)
-        {
-            arr[k] = (n_fact) / (factorial(k) * factorial(n - k));
-        }
-        return arr;
+        fscanf(fin, "%d", &x);
+        fclose(fin);
     }
-    return NULL;
+    return x;
+}
+double* binom (int x)
+{
+    double* arr = NULL;
+    int size = x+1;
+    arr = (double*)malloc(sizeof(double) * size);
+    if (arr && x >= 1)
+    {
+        arr[0] = 1;
+        arr[1] = 1;
+        arr[size-1] = 1;
+        int temp_1 = 1, temp_2 = 1;
+        for(int i = 2; i < size; i++)
+        {
+            arr[i-1] = 1;
+            arr[0] = 1;
+            for(int j = 1; j < i; j++)
+            {
+                if (j % 2 != 0)
+                {
+                    temp_1 = arr[j];
+                    arr[j] = temp_1 + temp_2;
+                }
+                else
+                {
+                    temp_2 = arr[j];
+                    arr[j] = temp_1 + temp_2;
+                }
+            }
+            temp_2 = 1;
+        }
+    }
+    return arr;
+}
+void write_to_file (const char* filename, double* arr, int size)
+{
+    FILE* fout = fopen(filename, "w");
+    if (fout)
+    {
+        fprintf(fout, "%d ", size);
+        for(int i = 0; i < size; i++)
+        {
+            fprintf(fout, "%g ", arr[i]);
+        }
+        fclose(fout);
+    }
 }
 int main(int argc, const char* argv[])
 {
-    if (3) {
-        int n = 0;
-        FILE* fin = fopen(argv[1], "r");
-        FILE* fout = fopen(argv[2], "w");
-        if (fin && fout)
-        {
-            if (fscanf(fin, "%d", &n) > 0 && n >= 0)
-            {
-                {
-                    double* arr = binom(n);
-                    fprintf(fout, "%d ", (n + 1));
-                    for (int i = 0; i < n + 1; i++)
-                    {
-                        fprintf(fout, "%g ", arr[i]);
-                    }
-                    free(arr);
-                }
-            }
-            else fprintf(fout, "0");
-            fclose(fout);
-            fclose(fin);
-        }
+    if (3)
+    {
+        int x = get_num_from_file("in.txt");
+        double* arr = binom(x);
+        write_to_file("out.txt", arr, x+1);
+        free(arr);
     }
     return  0;
 }
