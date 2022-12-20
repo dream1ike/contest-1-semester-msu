70abcef35fd3e0efeb59548b9a4fa43e2e2cc5a2
dream1ike <timur.uv@gmail.com>
2022-12-20 14:24:35 +0500

    binom complete

--- 2. working_with_arrays/10.binom/binom.cpp
+++ 2. working_with_arrays/10.binom/binom.cpp
@@ -7,22 +7,21 @@ double get_num_from_file(const char* filename)
     int x = 0;
     if (fin)
     {
-        fscanf(fin, "%d", &x);
+        if (fscanf(fin, "%d", &x) < 0) return -1;
         fclose(fin);
     }
     return x;
 }
 double* binom (int x)
 {
-    double* arr = NULL;
     int size = x+1;
-    arr = (double*)malloc(sizeof(double) * size);
-    if (arr && x >= 1)
+    double* arr = (double*)malloc(sizeof(double) * size);
+    if(arr)
     {
         arr[0] = 1;
         arr[1] = 1;
         arr[size-1] = 1;
-        int temp_1 = 1, temp_2 = 1;
+        double temp_1 = 1, temp_2 = 1;
         for(int i = 2; i < size; i++)
         {
             arr[i-1] = 1;
@@ -42,29 +41,41 @@ double* binom (int x)
             }
             temp_2 = 1;
         }
-    }
+    } 
     return arr;
 }
-void write_to_file (const char* filename, double* arr, int size)
+int write_to_file (const char* filename, double* arr, int size)
 {
     FILE* fout = fopen(filename, "w");
     if (fout)
     {
+        if (size == 0)
+        {
+            fprintf(fout, "0");
+            return 0;
+        }
         fprintf(fout, "%d ", size);
         for(int i = 0; i < size; i++)
         {
-            fprintf(fout, "%g ", arr[i]);
+            fprintf(fout, "%.0lf ", arr[i]);
         }
         fclose(fout);
     }
+    return 0;
 }
 int main(int argc, const char* argv[])
 {
-    if (3)
+    if (argc == 3)
     {
-        int x = get_num_from_file("in.txt");
+        int x = get_num_from_file(argv[1]);
+        int size = x+1;
         double* arr = binom(x);
-        write_to_file("out.txt", arr, x+1);
+        if (x == -1)
+        {
+             write_to_file (argv[2], arr, 0);
+             return 0;
+        }
+        write_to_file(argv[2], arr, size);
         free(arr);
     }
     return  0;
