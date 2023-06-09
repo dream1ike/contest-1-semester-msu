253bdd1e769c1f98ff564366b1d5ab9e6f49fef7
dream1ike <120911873+dream1ike@users.noreply.github.com>
2023-06-09 17:03:20 +0500

    полностью рабочий save.c

--- discrete_math/2. k-digit logic/save.c
+++ discrete_math/2. k-digit logic/save.c
@@ -1,16 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
+#include <string.h>
+#pragma warning(disable:4996)
 
-int check_number_in_set(int* set, int set_size, int number)
-{
-    for(int i = 0; i < set_size; i++)
-    {
-        if (number == set[i]) return 1;
+//основные функции:
+int* read_vector_table(const char* filename, int* k, int* n, int* vec_size);
+
+int** generate_subsets(int k, int n);
+
+int check_number_in_set(int* set, int set_size, int number);
+
+int convert_to_decimal(int* arr, int k, int n);
+
+void generate_combinations(int* set, int set_size, int* combination, int n, int index, int* vec_table, int table_size, int k, int* is_in_set);
+
+int compare(int* arr1, int* arr2);
+
+void bubble_sort(int** sets, int subset_count);
+
+
+int main(int argc, const char* argv[]) {
+    if (argc == 3) {
+        FILE* fout = fopen(argv[2], "w");
+        int k, n, table_size;
+        int is_in_set = 1;
+        int* vec_table = read_vector_table(argv[1], &k, &n, &table_size);
+        int** subsets = generate_subsets(k, n);
+        int subset_size = (1 << k) - 2;
+        int* combination = (int*)malloc(n * sizeof(int));
+        bubble_sort(subsets, subset_size);
+        for (int i = 0; i < subset_size; i++)
+        {
+            
+            generate_combinations(
+                &subsets[i][1], subsets[i][0],
+                combination, n, 0, vec_table, table_size, k, &is_in_set);
+            if (is_in_set)
+            {
+                for (int j = 1; j <= subsets[i][0]; j++)
+                {
+                    fprintf(fout, "%d", subsets[i][j]);
+                }
+                fprintf(fout, "\n");
+            }
+            is_in_set = 1;
+        }
+        free(vec_table);
     }
     return 0;
 }
 
+
+
+//основные функции:
 int* read_vector_table(const char* filename, int* k, int* n, int* vec_size) {
     FILE* fin = fopen(filename, "r");
     char trash;
@@ -26,115 +70,95 @@ int* read_vector_table(const char* filename, int* k, int* n, int* vec_size) {
         if (trash >= 65) table[i] = trash - 55; // расшифровываем буквы
         else table[i] = trash - '0';
     }
-    
+
     return table;
 }
 
-int count_bits(int n) {
-    int count = 0;
-    while(n) {
-        count += n & 1;
-        n >>= 1;
-    }
-    return count;
-}
 
 int** generate_subsets(int k, int n) {
-    int num_sets = 1 << k;
-    int **sets = (int**)malloc(num_sets * sizeof(int*));
-    for(int i = 0; i < num_sets; i++) {
-        int *set = (int*)malloc((n+1) * sizeof(int));
+    int num_sets = (1 << k) - 2;
+    int** sets = (int**)malloc(num_sets * sizeof(int*));
+    for (int i = 1; i <= num_sets; i++) {
+        int* set = (int*)malloc((n + 1) * sizeof(int));
         int set_size = 0;
-        for(int j = 0; j < k; j++) {
-            if(i & (1 << j)) {
-                set[set_size+1] = j;
+        for (int j = 0; j < k; j++) {
+            if (i & (1 << j)) {
+                set[set_size + 1] = j;
                 set_size++;
             }
         }
         set[0] = set_size;
-        sets[i] = set;
+        sets[i-1] = set;
     }
     return sets;
 }
 
+int compare(int* arr1, int* arr2) {
+    int len1 = arr1[0];
+    int len2 = arr2[0];
 
-
-void free_two_demensional_int_arr(int** arr, int sizeofarr) // done
-{
-    for (int i = 0; i < sizeofarr; i++) // очистка массива
-    {
-        free(arr[i]);
+    int min_len;
+    if (len1 < len2) {
+        min_len = len1;
     }
-    free(arr);
+    else {
+        min_len = len2;
+    }
+
+    for (int i = 1; i <= min_len; i++) {
+        if (arr1[i] != arr2[i]) {
+            return arr1[i] - arr2[i];
+        }
+    }
+    return len1 - len2;
+}
+
+void bubble_sort(int** sets, int subset_count) {
+    for (int i = 0; i < subset_count - 1; i++) {
+        for (int j = 0; j < subset_count - 1 - i; j++) {
+            if (compare(sets[j], sets[j + 1]) > 0) {
+                int* temp = sets[j];
+                sets[j] = sets[j + 1];
+                sets[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int check_number_in_set(int* set, int set_size, int number)
+{
+    for (int i = 0; i < set_size; i++)
+    {
+        if (number == set[i]) return 1;
+    }
+    return 0;
 }
 
 int convert_to_decimal(int* arr, int k, int n) {
     int result = 0;
-    for(int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         result += arr[i] * pow(k, n - i - 1);
     }
     return result;
 }
 
-void print_arr(int* arr, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
-
-void generate_combinations_and_check(int* set, int set_size, int* vec_table, int table_size, int* combination, int n, int index, int k, int* is_in_set) {
-    if(index == n) {
-        // Если достигнута нужная длина комбинация, возвращаем ее
+void generate_combinations(int* set, int set_size, int* combination, int n, int index, int* vec_table, int table_size, int k, int* is_in_set) {
+    if (index == n) {
+        // Если достигнута нужная длина комбинации, делаем проверку на вхождение
         if (*is_in_set)
         {
-            int index = convert_to_decimal(combination, k, set_size); // получили индекс нужного нам набора
-            *is_in_set = check_number_in_set(set, set_size, vec_table[index]);
-            print_arr(combination, set_size);
+        int elem_index = convert_to_decimal(combination, k, n);
+        *is_in_set = check_number_in_set(set, set_size, vec_table[elem_index]);
+
         }
         return;
     }
     // Проходим по всем элементам множества и добавляем каждый из них на текущую позицию
-    for(int i = 0; i < set_size; i++) {
+    for (int i = 0; i < set_size; i++) {
         combination[index] = set[i];
-        generate_combinations_and_check(set, set_size, vec_table, table_size, combination, n, index + 1, k, is_in_set);
+        generate_combinations(set, set_size, combination, n, index + 1, vec_table, table_size, k, is_in_set);
     }
 }
 
 
-int main(int argc, const char* argv[]) {
-    FILE* fout = fopen("/Users/timurbajdadaev/Visual_Studio_Code/Contest/file1.txt", "w");
-    if (3){
-    int k,n;
-    int table_size;
-    int* vec_table = read_vector_table("/Users/timurbajdadaev/Visual_Studio_Code/Contest/file2.txt",&k, &n, &table_size);
-    printf("k: %d\nn: %d\n", k,n);
-    for(int i = 0; i < table_size; i++)
-    {
-        printf("%d ", vec_table[i]);
-    }
-    
-    
-    printf("\n");
-    int subset_size = 1 << (k); 
-    int **sets = generate_subsets(k,n);
-    int* combination = (int*)malloc(n * sizeof(int));
-    int is_in_set = 1;
-    printf("\n");
 
-    // проверка на вхождение
-    for (int i = 1; i < subset_size - 1; i++)
-    {
-        generate_combinations_and_check(&sets[i][1], sets[i][0], vec_table, table_size, combination, n, 0, k, &is_in_set);
-        printf("--------\n");
-        print_arr(&sets[i][1], sets[i][0]);
-        printf("--------\n");
-        is_in_set = 1;
-    }
-    free_two_demensional_int_arr(sets, subset_size);
-    fclose(fout);
-    }
-    return 0;
-}
