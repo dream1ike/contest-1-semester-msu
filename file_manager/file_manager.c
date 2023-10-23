2b98618b8246da7d7085dd6c30799ffe6315869f
dream1ike <120911873+dream1ike@users.noreply.github.com>
2023-10-23 22:14:39 +0500

    ЗАРАБОТАЛА ДЕВОЧКА (add  my_create, my_create_dir)

--- file_manager/file_manager.c
+++ file_manager/file_manager.c
@@ -2,14 +2,14 @@
 #include "os_file.h"
 
 // Структура Элемента
-typedef struct
+typedef struct element
 {
     char* name;
     int size;
     char isfile;
     int number_of_kids;
-    element** kids;
-    element* parent;
+    struct element** kids;
+    struct element* parent;
 
 } element;
 
@@ -32,7 +32,7 @@ char** parser(char* path, int number_of_slashes) // returns arr with dir names (
 
 element* create_element(char* name, int size, int isfile, element* parent);
 
-element* find_elem(char** directoryNames, element* root) // returns elem, that we trying to find (unsafe) (helped go_to_path)
+element* find_elem(char** directoryNames, int size_directoryNames, element* root) // returns elem, that we trying to find (unsafe) (helped go_to_path)
 
 element* go_to_path(char* path); // main func, returns elem in path (second last), that we trying to find (safe)
 
@@ -70,12 +70,12 @@ void setup_file_manager(file_manager_t *fm) {
 // ---------------------------------------------
 
 // Supporting func
-int is_valid_path(char* path) // worked!
+int is_valid_path(const char* path) // worked!
 {
     int last_slash_index = -2; // -2 чтобы уж точно не было случайной разницы между индексами
     if (strlen(path) == 0) return 0;
     int count_slashes = 0;
-    for(int i = 0; i < strlen(path); i++)
+    for (int i = 0; i < strlen(path); i++)
     {
         // проверка, состоит ли пусть только из цифр, букв, / _ .
         // '.'       46
@@ -84,101 +84,110 @@ int is_valid_path(char* path) // worked!
         // 'A' - 'Z' 65 - 90 
         // '_'       95
         // 'a' - 'z' 97 - 122
-        if ((path[i] < 46) || ((57 < path[i]) && (path[i] < 65)) || ((90 < path[i]) && (path[i] < 95)) 
-         || (path[i] > 122) && (path[i] != 96)) return 0;
+        if ((path[i] < 46) || ((57 < path[i]) && (path[i] < 65)) || ((90 < path[i]) && (path[i] < 95))
+            || (path[i] > 122) && (path[i] != 96)) return 0;
 
         // проверка на правильность /
-        if (path[i] == '/') 
+        if (path[i] == '/')
         {
             count_slashes++;
-            // если встретятся два слеша подряд
+            // если встрет€тс€ два слеша подр€д
             if (abs(last_slash_index - i) == 1) return 0;
             last_slash_index = i;
         }
         // проверка на правильность .. и .
         if (path[i] == '.')
         {
-            int j = i+1;
+            int j = i + 1;
             int counter = 1;
-            for(; j < strlen(path) && path[j] == '.'; j++) counter++;
+            for (; j < strlen(path) && path[j] == '.'; j++) counter++;
             if (counter > 2) return 0;
         }
     }
-    // Обработка случая "/dir1/dir2/", когда есть слеш в конце
-    if (path[strlen(path)-1] == '/') count_slashes--;
+    // ќбработка случа€ "/dir1/dir2/", когда есть слеш в конце
+    if (path[strlen(path) - 1] == '/') count_slashes--;
     return count_slashes;
 }
 
-int is_path_absolute(char* path)
+int is_path_absolute(const char* path) // checked
 {
     return path[0] == '/';
 }
 
-char* get_first_dirname_from_path(char* path, int* t) // t - место, откуда нужно начать парсить (после слеша)
+char* get_first_dirname_from_path(const char* path, int* t) // t - место, откуда нужно начать парсить (после слеша) (checked)
 {
     char name_dir_static[33];
     int size_of_name_dir = 0;
-    // Копируем имя во временный массив
-    while ((path[*t] != '/') && (*t < strlen(path)))
+    //  опируем им€ во временный массив
+    while (((path[*t] != '/') || (*t == 0)) && (*t < strlen(path)))
     {
         name_dir_static[size_of_name_dir] = path[*t];
         size_of_name_dir++;
+        if ((*t == 0) && (path[*t] == '/')) break;
         *t += 1;
     }
-    // Пропускаем слеш
+    // ѕропускаем слеш
     *t += 1;
-    char* dirName = (char*)malloc(sizeof(char) * size_of_name_dir);
+    char* dirName = (char*)malloc(sizeof(char) * (size_of_name_dir+1));
     for (int i = 0; i < size_of_name_dir; i++)
     {
         dirName[i] = name_dir_static[i];
     }
+    dirName[size_of_name_dir] = '\0';
     return dirName;
-    
+
 }
 
-char** parser(char* path, int number_of_slashes, char** last_dir_name)
+char** parser(const char* path, int number_of_slashes, char** last_dir_name) // checked
 {
     char** directoryNames = (char**)malloc(sizeof(char*) * number_of_slashes);
     int i = 0;
-    if (path[0] == '/')
-    {
-        directoryNames[0] = (char*)malloc(sizeof(char));
-        directoryNames[0][0] = '/';
-        i++;
-    }
-    // Временная переменная, которая нужна для вложенного while
+    // t отвечает за перемещение по строке, которую хотим запарсить
     int t = i;
-    // i < number_of_slashes потому, что мы специально не считываем последнюю директорию
-    // это нужно для create_dir
+
+    // i < number_of_slashes-1 потому, что мы специально не считываем последнюю директорию
+    // это нужно дл€ create_dir
     while (i < number_of_slashes)
     {
         directoryNames[i] = get_first_dirname_from_path(path, &t);
         i++;
     }
-    // имя последней директории (нужно для create_dir)
+    // им€ последней директории (нужно дл€ create_dir)
     *last_dir_name = get_first_dirname_from_path(path, &t);
     return directoryNames;
 }
 
-element* find_elem(char** directoryNames, element* root)
+element* find_elem(char** directoryNames, int size_directoryNames, element* root)
 {
-    // Возможно рекурсивная функция. Подумай
+    element* result_directory = root;
+    for (int i = 0; i < size_directoryNames; i++)
+    {
+        for (int j = 0; j < result_directory->number_of_kids; j++)
+        {
+            element* kid = result_directory->kids[j];
+            if (!strcmp(directoryNames[i], kid->name))
+            {
+                // ≈сли нашли директорию с таким именем
+                result_directory = kid;
+                break;
+            }
+        }
+        // ≈сли не нашли нужную директорию
+        if (strcmp(directoryNames[i], result_directory->name)) return 0;
+    }
+    return result_directory;
 }
 
-element* go_to_path(char* path, char** last_dir_name) // last_dir_name нужен для create_dir
+element* go_to_path(const char* path, char** last_dir_name) // last_dir_name нужен дл€ create_dir
 {
     element* root = MANAGER.current_dir;
+    if (is_path_absolute(path)) root = MANAGER.root;
     int number_of_slashes = is_valid_path(path);
     if (number_of_slashes)
     {
-        if (is_path_absolute(path))
-        {
-            root = MANAGER.root;
-            number_of_slashes--;
-        }
         char** directoryNames = parser(path, number_of_slashes, last_dir_name);
         if (!directoryNames) return 0;
-        return find_elem(directoryNames, root);
+        return find_elem(directoryNames, number_of_slashes, root);
     }
     else
     {
@@ -197,6 +206,13 @@ element* create_element(char* name, int size, int isfile, element* parent)
     elem->parent = parent;
     return elem;
 }
+
+void add_kid_to_kids(element* parent, element* kid)
+{
+    parent->number_of_kids++;
+    parent->kids = (element**)realloc(parent->kids, parent->number_of_kids);
+    parent->kids[parent->number_of_kids - 1] = kid;
+}
 // ---------------------------------------------
 
 // My func
@@ -204,14 +220,21 @@ int my_create_dir(const char* path)
 {
     char* last_dir_name;
     element* curr_dir = go_to_path(path, &last_dir_name);
-    curr_dir->number_of_kids++;
+    if (!curr_dir) return 0;
     element* new_dir = (element*)malloc(sizeof(element));
-
+    add_kid_to_kids(curr_dir, new_dir);
+    new_dir->name = last_dir_name;
+    new_dir->size = 0;
+    new_dir->isfile = 0;
+    new_dir->number_of_kids = 0;
+    new_dir->kids = NULL;
+    new_dir->parent = curr_dir;
+    return 1;
 }
 
 int my_create(int disk_size)
 {
-    // Проверка, если менеджер уже создавался
+    // ѕроверка, если менеджер уже создавалс€
     if (MANAGER.root) return 0;
     MANAGER.size = disk_size;
     element* root = create_element("/", 0, 0, NULL);
